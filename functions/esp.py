3f8095c0b9aee652f8f0ef35a12d16830cf18689
0zean <npicini@gmail.com>
2024-06-23 02:29:24 -0400

    Updated esp.py

--- functions/esp.py
+++ functions/esp.py
@@ -29,6 +29,7 @@ offsets_dict = {
     'm_iTeamNum': nv.get('C_BaseEntity', 'm_iTeamNum'),
     'm_iHealth': nv.get('C_BaseEntity', 'm_iHealth'),
     'm_iszPlayerName': nv.get("CBasePlayerController", "m_iszPlayerName"),
+    'm_pBoneArray': 480
 }
 
 offsets.add_offsets(offsets_dict)
@@ -50,6 +51,14 @@ def w2s(mtx, posx, posy, posz, width, height):
         return [x, y]
 
     return [-999, -999]
+
+
+def bonePos(pm, bone, bone_matrix):    
+    x = pm.read_float(bone_matrix + bone * 0x20)
+    y = pm.read_float(bone_matrix + bone * 0x20 + 0x4)
+    z = pm.read_float(bone_matrix + bone * 0x20 + 0x8)
+
+    return [x, y, z]
 
 
 # ESP function
@@ -109,7 +118,7 @@ def pre_esp(pm, client, draw_list):
         entity_team = pm.read_int(entity_pawn_addr + offsets.m_iTeamNum)
         if entity_team == local_player_team:
               continue
-          
+
         player_name = pm.read_string(entity_controller + offsets.m_iszPlayerName, 32)
         player_name = player_name.split("\x00")[0]
           
@@ -121,47 +130,90 @@ def pre_esp(pm, client, draw_list):
         bone_matrix = pm.read_longlong(game_scene + offsets.m_modelState + 0x80)
 
         try:
+            cou_bone = bonePos(pm, 5, bone_matrix)
+            shoulderR_bone = bonePos(pm, 8, bone_matrix)
+            shoulderL_bone = bonePos(pm, 13, bone_matrix)
+            brasR_bone = bonePos(pm, 9, bone_matrix)
+            brasL_bone = bonePos(pm, 14, bone_matrix)
+            handR_bone = bonePos(pm, 11, bone_matrix)
+            handL_bone = bonePos(pm, 16, bone_matrix)
+            waist_bone = bonePos(pm, 0, bone_matrix)
+            kneesR_bone = bonePos(pm, 23, bone_matrix)
+            kneesL_bone = bonePos(pm, 26, bone_matrix)
+            feetR_bone = bonePos(pm, 24, bone_matrix)
+            feetL_bone = bonePos(pm, 27, bone_matrix)
             
-                headX = pm.read_float(bone_matrix + 6 * 0x20)
-                headY = pm.read_float(bone_matrix + 6 * 0x20 + 0x4)
-                headZ = pm.read_float(bone_matrix + 6 * 0x20 + 0x8) + 8
+            cou = w2s(view_matrix, cou_bone[0], cou_bone[1], cou_bone[2], ScreenY, ScreenX)
+            shoulderR = w2s(view_matrix, shoulderR_bone[0], shoulderR_bone[1], shoulderR_bone[2], ScreenY, ScreenX)
+            shoulderL = w2s(view_matrix, shoulderL_bone[0], shoulderL_bone[1], shoulderL_bone[2], ScreenY, ScreenX)
+            brasR = w2s(view_matrix, brasR_bone[0], brasR_bone[1], brasR_bone[2], ScreenY, ScreenX)
+            brasL = w2s(view_matrix, brasL_bone[0], brasL_bone[1], brasL_bone[2], ScreenY, ScreenX)
+            handR = w2s(view_matrix, handR_bone[0], handR_bone[1], handR_bone[2], ScreenY, ScreenX)
+            handL = w2s(view_matrix, handL_bone[0], handL_bone[1], handL_bone[2], ScreenY, ScreenX)
+            waist = w2s(view_matrix, waist_bone[0], waist_bone[1], waist_bone[2], ScreenY, ScreenX)
+            kneesR = w2s(view_matrix, kneesR_bone[0], kneesR_bone[1], kneesR_bone[2], ScreenY, ScreenX)
+            kneesL = w2s(view_matrix, kneesL_bone[0], kneesL_bone[1], kneesL_bone[2], ScreenY, ScreenX)
+            feetR = w2s(view_matrix, feetR_bone[0], feetR_bone[1], feetR_bone[2], ScreenY, ScreenX)
+            feetL = w2s(view_matrix, feetL_bone[0], feetL_bone[1], feetL_bone[2], ScreenY, ScreenX)
 
-                head_pos = w2s(view_matrix, headX, headY, headZ, ScreenY, ScreenX)
+            draw_list.add_line(cou[0], cou[1], shoulderR[0], shoulderR[1], color, 1)
+            draw_list.add_line(cou[0], cou[1], shoulderL[0], shoulderL[1], color, 1)
+            draw_list.add_line(brasL[0], brasL[1], shoulderL[0], shoulderL[1], color, 1)
+            draw_list.add_line(brasR[0], brasR[1], shoulderR[0], shoulderR[1], color, 1)
+            draw_list.add_line(brasR[0], brasR[1], handR[0], handR[1], color, 1)
+            draw_list.add_line(brasL[0], brasL[1], handL[0], handL[1], color, 1)
+            draw_list.add_line(cou[0], cou[1], waist[0], waist[1], color, 1)
+            draw_list.add_line(kneesR[0], kneesR[1], waist[0], waist[1], color, 1)
+            draw_list.add_line(kneesL[0], kneesL[1], waist[0], waist[1], color, 1)
+            draw_list.add_line(kneesL[0], kneesL[1], feetL[0], feetL[1], color, 1)
+            draw_list.add_line(kneesR[0], kneesR[1], feetR[0], feetR[1], color, 1)
+            
+            headX = pm.read_float(bone_matrix + 6 * 0x20)
+            headY = pm.read_float(bone_matrix + 6 * 0x20 + 0x4)
+            headZ = pm.read_float(bone_matrix + 6 * 0x20 + 0x8) + 8
 
-                legZ = pm.read_float(bone_matrix + 28 * 0x20 + 0x8)
+            head_pos = w2s(view_matrix, headX, headY, headZ, ScreenY, ScreenX)
 
-                leg_pos = w2s(view_matrix, headX, headY, legZ, ScreenY, ScreenX)
+            legZ = pm.read_float(bone_matrix + 28 * 0x20 + 0x8)
 
-                deltaZ = abs(head_pos[1] - leg_pos[1])
+            leg_pos = w2s(view_matrix, headX, headY, legZ, ScreenY, ScreenX)
 
-                leftX = head_pos[0] - deltaZ // 3.5
-                rightX = head_pos[0] + deltaZ // 3.5
-                
-                topY = head_pos[1] - 12
-                bottomY = head_pos[1] + deltaZ
+            deltaZ = abs(head_pos[1] - leg_pos[1])
 
-                draw_list.add_line(leftX, head_pos[1], rightX, head_pos[1], color, 1)
-                draw_list.add_line(leftX, leg_pos[1], rightX, leg_pos[1], color, 1)
+            leftX = head_pos[0] - deltaZ // 3.5
+            rightX = head_pos[0] + deltaZ // 3.5
+            
+            topY = head_pos[1] - 12
+            bottomY = head_pos[1] + deltaZ
 
-                draw_list.add_line(leftX, head_pos[1], leftX, leg_pos[1], color, 1)
-                draw_list.add_line(rightX, head_pos[1], rightX, leg_pos[1], color, 1)
+            draw_list.add_line(leftX, head_pos[1], rightX, head_pos[1], color, 1)
+            draw_list.add_line(leftX, leg_pos[1], rightX, leg_pos[1], color, 1)
 
-                # Health Bar
-                scaled_health_pos = head_pos[1] + ((100 - health) / 100.0) * deltaZ
-                
-                if health >= 70:
-                    health_color = imgui.get_color_u32_rgba(0, 128, 0, 1)
-                elif 70 > health > 30:
-                    health_color = imgui.get_color_u32_rgba(255, 140, 0, 1)
-                else:
-                    health_color = imgui.get_color_u32_rgba(255, 0, 0, 1)
+            draw_list.add_line(leftX, head_pos[1], leftX, leg_pos[1], color, 1)
+            draw_list.add_line(rightX, head_pos[1], rightX, leg_pos[1], color, 1)
+            
+            # Head circle
+            head_bone = bonePos(pm, 6, bone_matrix)
+            head_bone_pos = w2s(view_matrix, head_bone[0], head_bone[1], head_bone[2], ScreenY, ScreenX)
+            
+            draw_list.add_circle(head_bone_pos[0], head_bone_pos[1], abs(head_pos[1] - cou[1]) / 2, color)
 
-                # Draw the health bar
-                draw_list.add_line(leftX - 5, scaled_health_pos, leftX - 5, leg_pos[1], health_color, 2)
-                draw_list.add_text(leftX, bottomY, imgui.get_color_u32_rgba(1,1,0,1), f"HP: {health}")
-                
-                # Player Name
-                draw_list.add_text(leftX, topY, imgui.get_color_u32_rgba(1,1,0,1), player_name)
+            # Health Bar
+            scaled_health_pos = head_pos[1] + ((100 - health) / 100.0) * deltaZ
+            
+            if health >= 70:
+                health_color = imgui.get_color_u32_rgba(0, 128, 0, 1)
+            elif 70 > health > 30:
+                health_color = imgui.get_color_u32_rgba(255, 140, 0, 1)
+            else:
+                health_color = imgui.get_color_u32_rgba(255, 0, 0, 1)
+
+            # Draw the health bar
+            draw_list.add_line(leftX - 5, scaled_health_pos, leftX - 5, leg_pos[1], health_color, 2)
+            draw_list.add_text(leftX, bottomY, imgui.get_color_u32_rgba(1,1,0,1), f"HP: {health}")
+            
+            # Player Name
+            draw_list.add_text(leftX, topY, imgui.get_color_u32_rgba(1,1,0,1), player_name)
 
         except Exception:
             print("cant get bones")
